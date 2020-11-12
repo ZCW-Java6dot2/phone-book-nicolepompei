563b8054f4f0b548fb42cb22eae32695370f2edc
pompy <nicole.lynn.pompei@gmail.com>
2020-11-12 17:43:13 -0500

    completed phone book lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,11 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
+import java.util.*;
+import java.util.HashMap;
 import java.util.ArrayList;
-//import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
+
+
 
 /**
  * Created by leon on 1/23/18.
@@ -13,42 +13,90 @@ import java.util.Map;
  */
 public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
+    private String name;
+    private String phoneNumber;
+    private List<String> phoneNumbersList = new ArrayList<>();
+    private LinkedHashMap<String, List<String>> phonebook = new LinkedHashMap<>();
 
-    public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+    public PhoneBook(LinkedHashMap<String, List<String>> map) {
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+
     }
 
     public void add(String name, String phoneNumber) {
+        phoneNumbersList.add(phoneNumber);
+        phonebook.put(name, phoneNumbersList);
+
     }
 
+    /**
+     * converts array of phone number strings to an array list, adds array list of phone numbers to
+     * appropriate name in phonebook list
+     *
+     * @param name
+     * @param phoneNumbers
+     */
     public void addAll(String name, String... phoneNumbers) {
+        List<String> allPhoneNumbers =  Arrays.asList(phoneNumbers);
+        phonebook.put(name, allPhoneNumbers);
     }
+
 
     public void remove(String name) {
+        this.phonebook.remove(name);
+
     }
 
+    /**
+     * @param name
+     * @return
+     */
     public Boolean hasEntry(String name) {
-        return null;
+        Boolean result;
+
+        if(this.phonebook.containsKey(name) || this.phoneNumbersList.contains(name)){
+            result = true;
+
+        } else result = false;
+        return result;
     }
+
+
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
+        /**
+         * get all treemap entries w/ entrySet
+         * define value for which we need to fetch key (param in this case)
+         * loop through all values of treemap and return the key that matches the inputted value
+         * @param phoneNumber
+         * @return
+         */
+        public String reverseLookup (String phoneNumber){
+
+            for(Map.Entry<String, List<String>> elements: phonebook.entrySet()){
+                if(elements.getValue().contains(phoneNumber)){
+                    return elements.getKey();
+                }
+
+            }
+            return null;
+        }
+
+        public List<String> getAllContactNames () {
+            List<String> listOfNames = new ArrayList(phonebook.keySet());
+            return listOfNames;
+
+        }
+
+        public Map<String, List<String>> getMap () {
+            return phonebook;
+        }
+
     }
 
-    public List<String> getAllContactNames() {
-        return null;
-    }
-
-    public Map<String, List<String>> getMap() {
-        return null;
-    }
-}
